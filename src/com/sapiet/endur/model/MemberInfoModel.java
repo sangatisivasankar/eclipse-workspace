fc5d8a9bcd2d057b51fff53dfd9efed205e1bc3d
sansivas <sangati.sivasankar@publicissapient.com>
2022-07-12 13:30:12 +0530

    first commit

---
+++ src/com/sapiet/endur/model/MemberInfoModel.java
@@ -0,0 +1,20 @@
+package com.sapiet.endur.model;
+import java.time.LocalDate;
+
+import com.sapient.endur.model.MemberInfoModel;
+
+public class MemberInfoModel implements Comparable<MemberInfoModel>{
+	private Long adharCard;
+	private String name;
+	private String address;
+	private String email;
+	private Long mobile;
+	private static String colonyName;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
